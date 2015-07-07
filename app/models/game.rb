cc4d121a2dfde9d3bd35268579380d048a18f32b
Polly Sileo <pollysileo@gmail.com>
2015-07-06 22:01:06 -0400

    Hangman class ported over as is

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,5 @@ class Game < ActiveRecord::Base
   belongs_to :player, class_name: "User"
   belongs_to :suggester, class_name: "User"
   validates_presence_of :secret_word, :player, :suggester
+
 end
--- app/models/hangman.rb
+++ app/models/hangman.rb
@@ -1,6 +1,43 @@
-class Hangman
+class HangmanGame
+  attr_reader :tries, :word, :guess_word
   def initialize
-    @guesses_left = 8
+      words = File.read("words.txt").lines
+      #chose a random word from words.txt
+      #@word = words.sample.strip
+      @word = words.sample.strip #just for dev so I know what the word id
+      @tries = 3
+      @guess_word = []
+      #should map here...
+      (@word.length).times do |blank|
+        @guess_word << "__ "
+      end
   end
-  
+
+  def over?
+    @tries <=0 || @guess_word.join == @word
+  end
+  def won?
+    @guess_word.join == @word
+  end
+
+  def lost?
+    @tries == 0
+  end
+  def compare guess_letter
+    #@guesses -= 1
+    word_array = @word.strip.split(//)
+    if word_array.include? guess_letter
+      puts 'Good guess'
+      #index_to_be_replaced = word_array.index(guess_letter)
+      match_index = word_array.each_index.select{|i| word_array[i] == guess_letter }
+      match_index.each do |index|
+        @guess_word[index] = guess_letter
+      end
+      #@guess_word[index_to_be_replaced] = guess_letter
+    else
+      puts "Bad guess"
+      @tries -= 1
+    end
+  end
+
 end
