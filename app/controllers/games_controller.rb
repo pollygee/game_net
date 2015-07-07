98a8c77bb3141ff2e42f4a6f2c1bcc7bacf3a7ef
Polly Sileo <pollysileo@gmail.com>
2015-07-07 17:29:36 -0400

    Creating games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,5 +5,8 @@ class GamesController < ApplicationController
   end
 
   def create
+    @player_id = params['game']['player_id'].to_i
+    @secret_word = params['game']['word']
+    Game.create! player_id: @player_id, suggester_id: current_user.id, secret_word: @secret_word
   end
 end
